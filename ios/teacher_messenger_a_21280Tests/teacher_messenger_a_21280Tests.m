31f265a1bb5897dca3e9c269d15ed1efda32cfc9
Crowdbotics <team@crowdbotics.com>
2020-10-09 00:23:24 +0000

    Initial commit.

---
+++ ios/teacher_messenger_a_21280Tests/teacher_messenger_a_21280Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface teacher_messenger_a_21280 : XCTestCase
+
+@end
+
+@implementation teacher_messenger_a_21280
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
